8fc2e5a998009a3078b00828037106c1a61804b0
radne0 <jmcornelius0@gmail.com>
2024-12-25 11:05:51 -0500

    update to main.py

--- main.py
+++ main.py
@@ -12,6 +12,28 @@ def countwords(txt):
     wordcount = len(words)
     return wordcount
 
+# create a dictionary of character counts...
+def countchars(txt):
+    chardict = {}
+
+    txt = txt.lower()
+    for ch in txt:
+        if ch not in chardict: 
+            chardict[ch]=0   
+        chardict[ch] +=1
+
+    return chardict
+
+def generatereport(path,words,chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"{words} words found in the document\n")
+
+
+
+
+    for k in chars:
+        if k.isalpha():
+            print(f"The \'{k}\' characeter was found {chars[k]} times")
 
 
 # read in a book, then count words..
@@ -19,7 +41,9 @@ def main():
     bookpath = "books/frankenstein.txt"
     booktext = readbook(bookpath)
     wordcount = countwords(booktext)
-    print(wordcount)    
+    charcount = countchars(booktext)
+    print(charcount)
+    generatereport(bookpath,wordcount,charcount)
 
 main()
 
