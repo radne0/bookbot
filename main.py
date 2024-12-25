b711f2560b5f383fdad9e17c593569f71a5afbae
radne0 <jmcornelius0@gmail.com>
2024-12-25 10:30:47 -0500

    First commit of main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+#read in a book from the specificed path
+def readbook(path):
+    with open(path) as f:
+        file_contents = f.read()         
+        return file_contents
+
+
+
+# countwords in a string
+def countwords(txt):
+    words = txt.split()
+    wordcount = len(words)
+    return wordcount
+
+
+
+# read in a book, then count words..
+def main(): 
+    bookpath = "books/frankenstein.txt"
+    booktext = readbook(bookpath)
+    wordcount = countwords(booktext)
+    print(wordcount)    
+
+main()
+
+
+
+
+
+
