59dd69fff44c2065d7e2ab77489451c59255f7e9
radne0 <jmcornelius0@gmail.com>
2024-12-26 00:56:24 -0500

    main.py final update (for now)

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
-#read in a book from the specificed path
+###################################################################################################
+# BookBot
+# Guided project boot.dev
+# Reads in a text file counts the number of words and the count for each letter in the document.
+###################################################################################################
+
+## readbook(path)
+# read in a book from the specificed string path
 def readbook(path):
     with open(path) as f:
         file_contents = f.read()         
         return file_contents
 
-
-
-# countwords in a string
+## countwords(txt)
+# return the number of words in the string txt.   
 def countwords(txt):
     words = txt.split()
     wordcount = len(words)
     return wordcount
 
-# create a dictionary of character counts...
+## countchars(txt)
+# return a list that countains the number of time each alphanumerical character appears in txt.
 def countchars(txt):
     chardict = {}
 
+    # create a dictionary from all characters and their count.
     txt = txt.lower()
     for ch in txt:
         if ch not in chardict: 
             chardict[ch]=0   
         chardict[ch] +=1
 
-    return chardict
+    # create a list of dictionaries from this so that it can be sorted...
+    charlist = []
+    for ch in chardict:
+        if ch.isalpha():
+            charlist.append( {"character": ch, "count":chardict[ch]  }           )
 
-def sort_on(dict):
-    return dict["num"]
+    # sort
+    charlist.sort(reverse=True,key=sort_on)
+    return charlist
+
+
+
+## sort_on()
+#  helper function for sorting the list of dictionaries.
+def sort_on(lst):
+    return lst["count"]
 
 
 def generatereport(path,words,chars):
     print(f"--- Begin report of {path} ---")
     print(f"{words} words found in the document\n")
 
-    wordlist = list(words)
-    print(wordlist)
-
-
-
-
-
-
-
-    for k in chars:
-        if k.isalpha():
-            print(f"The \'{k}\' characeter was found {chars[k]} times")
+    for d in chars:
+        ch = d["character"]
+        charcount = d["count"]
+        print(f"The \'{ch}\' characeter was found {charcount} times")
 
 
 # read in a book, then count words..
@@ -52,7 +64,6 @@ def main():
     booktext = readbook(bookpath)
     wordcount = countwords(booktext)
     charcount = countchars(booktext)
-    print(charcount)
     generatereport(bookpath,wordcount,charcount)
 
 main()
